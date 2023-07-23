ac8c154cc7e61f367f69205b62585ec1e4b7ae02
srimanchaudhuri <srimanchaudhuri@gmail.com>
2023-07-24 02:16:18 +0530

    Created Menu Item and Homepage --Added Charts from Recharts

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,16 @@
+import ChartBox from "../../components/chartbox/ChartBox"
+import TopBox from "../../components/topBox/TopBox"
 import "./home.scss"
 const Home = () => {
   return (
     <div className="home">
-      <div className="box box1">box1</div>
-      <div className="box box2">box2</div>
-      <div className="box box3">box3</div>
-      <div className="box box4">box4</div>
-      <div className="box box5">box5</div>
+      <div className="box box1">
+        <TopBox/>
+      </div>
+      <div className="box box2"><ChartBox/></div>
+      <div className="box box3"><ChartBox/></div>
+      <div className="box box4"><ChartBox/></div>
+      <div className="box box5"><ChartBox/></div>
       <div className="box box6">box6</div>
       <div className="box box7">box7</div>
       <div className="box box8">box8</div>
